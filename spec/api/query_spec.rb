2402b97ca9af267bcee75b23450a5ccaec509220
Clinton Blackburn <clinton.blackburn@gmail.com>
2016-01-01 23:40:27 -0500

    Updated Query spec
    -Using factories to generate data
    - Using single quotes

--- spec/api/query_spec.rb
+++ spec/api/query_spec.rb
@@ -1,59 +1,37 @@
 require 'spec_helper'
+require 'faker'
 
-describe "app" do
+describe 'app' do
 
-  before (:each) { set_api_key_header }
+  before(:each) { set_api_key_header }
+  let(:body) { Faker::Lorem.word }
 
-  let(:author) { create_test_user(1) }
-  describe "thread search" do
-    describe "GET /api/v1/search/threads" do
-      it "returns thread with query match" do
-        commentable = Commentable.new("question_1")
+  describe 'thread search' do
+    describe 'GET /api/v1/search/threads' do
+      it 'returns thread with query match' do
+        thread = create(:comment_thread, body: body)
+        refresh_es_index
 
-        random_string = (0...8).map{ ('a'..'z').to_a[rand(26)] }.join
-
-        thread = CommentThread.new(title: "Test title", body: random_string, course_id: "1", commentable_id: commentable.id)
-        thread.thread_type = :discussion
-        thread.author = author
-        thread.save!
-
-        sleep 3
-
-        get "/api/v1/search/threads", text: random_string
+        get '/api/v1/search/threads', text: body
         last_response.should be_ok
-        threads = parse(last_response.body)['collection']
-        check_thread_result_json(nil, thread, threads.select{|t| t["id"] == thread.id.to_s}.first)
+        result = parse(last_response.body)['collection'].select { |t| t['id'] == thread.id.to_s }.first
+        check_thread_result_json(nil, thread, result)
       end
 
     end
   end
 
-  describe "comment search" do
-    describe "GET /api/v1/search/threads" do
-      it "returns thread with comment query match" do
-        commentable = Commentable.new("question_1")
+  describe 'comment search' do
+    describe 'GET /api/v1/search/threads' do
+      it 'returns thread with comment query match' do
+        comment = create(:comment, body: body)
+        thread = comment.comment_thread
+        refresh_es_index
 
-        random_string = (0...8).map{ ('a'..'z').to_a[rand(26)] }.join
-
-        thread = CommentThread.new(title: "Test title", body: "elephant otter", course_id: "1", commentable_id: commentable.id)
-        thread.thread_type = :discussion
-        thread.author = author
-        thread.save!
-
-        sleep 3
-
-        comment = Comment.new(body: random_string, course_id: "1")
-        comment.commentable_id = commentable.id
-        comment.author = author
-        comment.comment_thread = thread
-        comment.save!
-
-        sleep 1
-
-        get "/api/v1/search/threads", text: random_string
+        get '/api/v1/search/threads', text: body
         last_response.should be_ok
-        threads = parse(last_response.body)['collection']
-        check_thread_result_json(nil, thread, threads.select{|t| t["id"] == thread.id.to_s}.first)
+        result = parse(last_response.body)['collection'].select { |t| t['id'] == thread.id.to_s }.first
+        check_thread_result_json(nil, thread, result)
       end
     end
   end
