7ae560e3626071732a79ed68ce98927c8f1eae98
dominicfollett <dominic@proversity.org>
2016-06-28 13:48:50 +0000

    Add private_to_peers field to model slicer, when forum creates a new ThreadPresenter Object.

--- models/comment_thread.rb
+++ models/comment_thread.rb
@@ -123,7 +123,7 @@ class CommentThread < Content
   end
 
   def to_hash(params={})
-    as_document.slice(*%w[thread_type title body course_id anonymous anonymous_to_peers commentable_id created_at updated_at at_position_list closed context])
+    as_document.slice(*%w[thread_type title body course_id anonymous anonymous_to_peers private_to_peers commentable_id created_at updated_at at_position_list closed context])
         .merge('id' => _id,
                'user_id' => author_id,
                'username' => author_username,
